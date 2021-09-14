adff1a9ee8f7daab8ddf58dfa5d6588debe7e652
your_username <MyEmail@Zoho.com>
2021-09-14 08:53:41 +0530

    fix - functionality in calculation

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ const tableSection = document.querySelector('.change-table');
 hideCashGivenAndTable()
 
 nextButton.addEventListener('click', () => {
-    if (billAmount.value > 0) {
-        displayCahGivenSection();
+    if (+billAmount.value > 0) {
+        errorText.style.display = 'none'
+        showCashGivenSection();
         checkButton.addEventListener('click', () => {
-            errorText.style.display = 'none'
-            // console.log(billAmount.value)
-            if (billAmount.value > 0) {
-                if (cashGiven.value >= billAmount.value) {
+            if (+cashGiven.value > 0) {
+                if (+cashGiven.value >= +billAmount.value) {
                     const amountToBeReturn = cashGiven.value - billAmount.value;
                     calculateChange(amountToBeReturn);
-                    showTable()
+                    showTable();
                 } else {
-                    showErrorMessage("Given Amount can't be less than Bill Amount.")
+                    showErrorMessage("Given Amount can't be less than Bill Amount.");
                 }
             } else {
-                showErrorMessage('Invalid Bill Amount')
+                showErrorMessage('Invalid Cash Amount');
+                tableSection.style.display = 'none';
             }
         })
     } else {
-        hideCashGivenSection()
+        /* when cash section cash is given and bill amount is not having appropiate */
+        hideCashGivenAndTable();
         showErrorMessage("Put a Valid Input !! ")
     }
 })
@@ -49,14 +50,12 @@ function showErrorMessage(msgText) {
     errorText.innerHTML = msgText;
 }
 
-function displayCahGivenSection() {
+function showCashGivenSection() {
     cashGivenSection.style.display = 'block'
 }
 
-function hideCashGivenSection() {
-    cashGivenSection.style.display = 'none'
-}
 function hideCashGivenAndTable() {
+    cashGiven.value = "";
     cashGivenSection.style.display = 'none'
     tableSection.style.display = 'none'
 }
@@ -64,58 +63,3 @@ function showTable() {
     tableSection.style.display = 'block'
 }
 
-
-/*
-function getValue() {
-
-    let billAmount = Number(document.getElementById('billAmount').value);
-    let cashGiven = Number(document.getElementById('cashGiven').value);
-    let cash = calculateValue(billAmount, cashGiven);
-    let arr = ['twoThoudand', 'fiveHundred', 'oneHundred', 'fifty', 'twenty', 'ten', 'five'];
-    for (let i = 0; i < arr.length; i++) {
-        document.getElementById(arr[i]).innerHTML = cash[i];
-    }
-}
-
-function calculateValue(billAmount, cashGiven) {
-    let getValue = cashGiven - billAmount;
-    let val = getValue;
-    console.log(val)
-    let arr = [0, 0, 0, 0, 0, 0, 0]
-    while (val >= 1) {
-
-        if (val >= 2000) {
-            let quo = Math.floor(val / 2000)
-            arr[0] = arr[0] + quo;
-            val = val % 2000
-        } else if (val >= 500) {
-            let quo = Math.floor(val / 500);
-            arr[1] = arr[1] + quo;
-            val = val % 500
-        } else if (val >= 100) {
-            let quo = Math.floor(val / 100);
-            arr[2] = arr[2] + quo;
-            val = val % 100
-        } else if (val >= 20) {
-            let quo = Math.floor(val / 20);
-            arr[3] = arr[3] + quo;
-            val = val % 20
-        } else if (val >= 10) {
-            let quo = Math.floor(val / 10);
-            arr[4] = arr[4] + quo;
-            val = val % 10
-        } else if (val >= 5) {
-            let quo = Math.floor(val / 5);
-            arr[5] = arr[5] + quo;
-            val = val % 5
-        } else if (val >= 1) {
-            let quo = Math.floor(val / 1);
-            arr[6] = arr[6] + quo;
-            val = val % 1
-        }
-        console.log("hello")
-
-    }
-    return arr;
-}
-*/
